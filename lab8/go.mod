e147f616bce3d6613e8fd986501223ff331b8b2a
s230233 <230233@edu.psuti.ru>
2025-10-14 20:37:35 +0400

    feat:lab8-2 Подклчение базы данных PostgreSql

--- lab8/go.mod
+++ lab8/go.mod
@@ -2,4 +2,7 @@ module lab8
 
 go 1.25.1
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- lab8/main.go
+++ lab8/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 // Структура пользователя
@@ -15,13 +18,29 @@ type User struct {
 	Age  int    `json:"age"`
 }
 
-var users []User
-var nextID = 1
+var db *sql.DB
 
 func main() {
-	r := mux.NewRouter()
+	// Подключение к базе данных
+	connStr := "user=postgres password=postgres dbname=usersdb sslmode=disable"
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-	// Маршруты
+	// Проверяем соединение
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Не удалось подключиться к базе данных:", err)
+	}
+	log.Println("Подключение к базе данных успешно!")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("REST API with PostgreSQL is running"))
+	})
 	r.HandleFunc("/users", getUsers).Methods("GET")
 	r.HandleFunc("/users/{id}", getUser).Methods("GET")
 	r.HandleFunc("/users", createUser).Methods("POST")
@@ -31,8 +50,21 @@ func main() {
 	http.ListenAndServe(":8080", r)
 }
 
-// Получить список всех пользователей
+// Получить всех пользователей
 func getUsers(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT id, name, age FROM users")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var users []User
+	for rows.Next() {
+		var user User
+		rows.Scan(&user.ID, &user.Name, &user.Age)
+		users = append(users, user)
+	}
 	json.NewEncoder(w).Encode(users)
 }
 
@@ -40,50 +72,56 @@ func getUsers(w http.ResponseWriter, r *http.Request) {
 func getUser(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id, _ := strconv.Atoi(params["id"])
-	for _, user := range users {
-		if user.ID == id {
-			json.NewEncoder(w).Encode(user)
-			return
-		}
+	var user User
+	err := db.QueryRow("SELECT id, name, age FROM users WHERE id = $1", id).Scan(&user.ID, &user.Name, &user.Age)
+	if err == sql.ErrNoRows {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	http.Error(w, "User not found", http.StatusNotFound)
+	json.NewEncoder(w).Encode(user)
 }
 
 // Добавить нового пользователя
 func createUser(w http.ResponseWriter, r *http.Request) {
 	var user User
 	json.NewDecoder(r.Body).Decode(&user)
-	user.ID = nextID
-	nextID++
-	users = append(users, user)
+	err := db.QueryRow(
+		"INSERT INTO users (name, age) VALUES ($1, $2) RETURNING id",
+		user.Name, user.Age,
+	).Scan(&user.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	json.NewEncoder(w).Encode(user)
 }
 
-// Обновить пользователя по ID
+// Обновить пользователя
 func updateUser(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id, _ := strconv.Atoi(params["id"])
-	for i, user := range users {
-		if user.ID == id {
-			json.NewDecoder(r.Body).Decode(&users[i])
-			users[i].ID = id
-			json.NewEncoder(w).Encode(users[i])
-			return
-		}
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	_, err := db.Exec("UPDATE users SET name=$1, age=$2 WHERE id=$3", user.Name, user.Age, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	http.Error(w, "User not found", http.StatusNotFound)
+	user.ID = id
+	json.NewEncoder(w).Encode(user)
 }
 
-// Удалить пользователя по ID
+// Удалить пользователя
 func deleteUser(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id, _ := strconv.Atoi(params["id"])
-	for i, user := range users {
-		if user.ID == id {
-			users = append(users[:i], users[i+1:]...)
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
+	_, err := db.Exec("DELETE FROM users WHERE id=$1", id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	http.Error(w, "User not found", http.StatusNotFound)
+	w.WriteHeader(http.StatusNoContent)
 }
